51f8e9e39ea9fc95b87fee1a1b1ada56c0cf6012
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-01-25 19:58:42 +0000

    Add comments to the examples.

--- examples/conway.rs
+++ examples/conway.rs
@@ -1,3 +1,5 @@
+//! An example implementing conways game of life on the surface of a sphere.
+
 use std::{error::Error, f64::consts::PI, mem::swap, time::{Instant, Duration}};
 
 use pixels::{SurfaceTexture, Pixels};
@@ -5,19 +7,23 @@ use rand::{thread_rng, Rng};
 use surface_grid::{sphere::{CubeSphereGrid, CubeSpherePoint, SpherePoint}, SurfaceGrid};
 use winit::{event_loop::{EventLoop, ControlFlow}, window::WindowBuilder, dpi::{LogicalSize, PhysicalSize}, event::{Event, WindowEvent, StartCause}};
 
+// The initial window size.
 const WINDOW_WIDTH: usize = 720;
 const WINDOW_HEIGHT: usize = 480;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    // This example uses winit with pixels to display the game.
     let event_loop = EventLoop::new()?;
 
     let size = LogicalSize::new(WINDOW_WIDTH as f64, WINDOW_HEIGHT as f64);
 
+    // Build the window.
     let window = WindowBuilder::new()
         .with_title("Conway's Game of Life")
         .with_inner_size(size)
         .build(&event_loop)?;
 
+    // Pixels setup.
     let window_size = window.inner_size();
     let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
 
@@ -25,6 +31,8 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let mut pixels = Pixels::new(window_size.width, window_size.height, surface_texture)?;
 
+    // Create two grids to swap between.
+    // This saves allocating a new grid for each frame.
     let mut rng = thread_rng();
 
     let mut buffer1: CubeSphereGrid<bool, 256> = CubeSphereGrid::from_fn(|_| rng.gen());
@@ -33,14 +41,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     event_loop.run(move |event, target| {
         match event {
             Event::NewEvents(StartCause::Init) => {
+                // Update at 60 FPS.
                 target.set_control_flow(ControlFlow::WaitUntil(Instant::now() + Duration::from_millis(1000 / 60)))
             },
             Event::NewEvents(StartCause::ResumeTimeReached { .. }) => {
+                // Redraw on each frame.
                 window.request_redraw();
             }
             Event::WindowEvent { event, .. } => {
                 match event {
                     WindowEvent::Resized(window_size) => {
+                        // Handle resizing.
                         if window_size.width != 0 && window_size.height != 0 {
                             size = PhysicalSize::new(window_size.width, window_size.height);
 
@@ -56,8 +67,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                         target.exit()
                     }
                     WindowEvent::RedrawRequested => {
-                        println!("Frame");
-
+                        // Calculate conways game of life in parallel.
                         buffer2.set_from_neighbours_diagonals_par(&buffer1, |s1, s2, s3, s4, current, s6, s7, s8, s9| {
                             let count = [s1, s2, s3, s4, s6, s7, s8, s9]
                                 .into_iter()
@@ -77,19 +87,25 @@ fn main() -> Result<(), Box<dyn Error>> {
                             }
                         });
 
+                        // Swap the buffers.
                         swap(&mut buffer2, &mut buffer1);
 
+                        // Display the result using pixels.
                         let frame = pixels.frame_mut();
                 
                         for y in 0..size.height {
                             for x in 0..size.width {
                                 let i = (y as usize * size.width as usize + x as usize) * 4;
 
+                                // Convert the X Y screen coordinates to an equirectangular
+                                // projection of the latitude and longitude.
                                 let latitude = (y as f64 / size.height as f64) * PI - PI / 2.0;
                                 let longitude = (x as f64 / size.width as f64) * PI * 2.0;
 
+                                // Gets the value stored at the latitude and longitude calculated.
                                 let value = buffer1[CubeSpherePoint::from_geographic(latitude, longitude)];
 
+                                // Set the pixel colour.
                                 if value {
                                     frame[i] = 255;
                                     frame[i + 1] = 255;
@@ -103,6 +119,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                             }
                         }
 
+                        // Render the pixels to the screen.
                         pixels.render().expect("Failed to render");
                     },
                     _ => {}
